6c98c0a7dc1ad7287ea8e9f857768eea4c7b11b9
Filipe-Rodrigues <filiperodrigues97@hotmail.com>
2019-01-10 13:49:41 -0200

    First commit

---
+++ src/engine/main/ApplicationMain.java
@@ -0,0 +1,10 @@
+package engine.main;
+
+import engine.core.ParticlesCore;
+
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        ParticlesCore core = new ParticlesCore(60000);
+    }
+}
